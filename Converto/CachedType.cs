c2fab4bd92e385fab7878dc1a0e666e8c9539a15
Odonno <bottiau.david@laposte.net>
2018-11-27 21:10:25 +0100

    feat: complete ConvertTo method

---
+++ Converto/CachedType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Converto
+{
+    internal class CachedConstructorInfo
+    {
+        public ConstructorInfo Constructor { get; }
+        public List<ParameterInfo> Parameters { get; }
+
+        public CachedConstructorInfo(ConstructorInfo constructorInfo)
+        {
+            Constructor = constructorInfo;
+            Parameters = constructorInfo.GetParameters().ToList();
+        }
+    }
+
+    internal class CachedTypeInfo
+    {
+        public Type Type { get; }
+        public List<CachedConstructorInfo> CachedPublicConstructors { get; }
+        public List<PropertyInfo> Properties { get; }
+        public List<PropertyInfo> ReadOnlyProperties { get; }
+        public List<PropertyInfo> WriteOnlyProperties { get; }
+
+        public CachedTypeInfo(Type type)
+        {
+            Type = type;
+
+            var typeInfo = type.GetTypeInfo();
+
+            var cachedConstructors =
+                typeInfo.DeclaredConstructors.Select(c => new CachedConstructorInfo(c)).ToList();
+
+            CachedPublicConstructors = cachedConstructors
+                                      .Where(cc => cc.Constructor.IsPublic && !cc.Constructor.IsStatic).ToList();
+
+            Properties = type.GetRuntimeProperties().ToList();
+            ReadOnlyProperties = Properties.Where(p => p.CanRead && !p.CanWrite).ToList();
+            WriteOnlyProperties = Properties.Where(p => !p.CanRead && p.CanWrite).ToList();
+        }
+    }
+}
---
+++ Converto/Main.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Converto
+{
+    public static class Main
+    {
+        private static readonly Dictionary<string, CachedTypeInfo> CachedTypeInfoDetails =
+            new Dictionary<string, CachedTypeInfo>();
+
+        public static T Copy<T>(this T @object) where T : class
+        {
+            // TODO
+        }
+        public static bool TryCopy<T>(this T @object, out T result) where T : class
+        {
+            // TODO
+        }
+
+        public static T With<T, TProps>(this T itemToCopy, TProps propertiesToUpdate)
+            where T : class where TProps : class
+        {
+            // TODO
+        }
+        public static bool TryWith<T, TProps>(this T itemToCopy, TProps propertiesToUpdate, out T result)
+            where T : class where TProps : class
+        {
+            // TODO
+        }
+
+        public static T ConvertTo<T>(this object @object) where T : class
+        {
+            if (@object == null)
+                return default(T);
+
+            if (@object is JObject jsonObject)
+                return jsonObject.ToObject<T>();
+
+            var objectTypeInfo = GetCachedTypeInfo(@object.GetType());
+            var targetTypeInfo = GetCachedTypeInfo(typeof(T));
+
+            var result = Activator.CreateInstance(objectTypeInfo.Type, false);
+
+            var targetPropertiesInfo = targetTypeInfo.Properties.Where(p => p.CanWrite);
+
+            foreach (var targetPropertyInfo in targetPropertiesInfo)
+            {
+                var objectPropertyInfo = objectTypeInfo.Properties.FirstOrDefault(p => p.Name == targetPropertyInfo.Name && p.CanRead);
+                if (objectPropertyInfo != null)
+                {
+                    var value = objectPropertyInfo.GetValue(@object, null);
+                    targetPropertyInfo.SetValue(result, value, null);
+                }
+            }
+
+            return (T)result;
+        }
+        public static bool TryConvertTo<T>(this object @object, out T result) where T : class
+        {
+            result = ConvertTo<T>(@object);
+            return result != null;
+        }
+
+        private static CachedTypeInfo GetCachedTypeInfo(Type type)
+            => CachedTypeInfoDetails.GetOrAddValue(type.FullName, () => new CachedTypeInfo(type));
+
+        private static T GetOrAddValue<T>(this Dictionary<string, T> dictionary, string key, Func<T> createValue)
+        {
+            if (dictionary.TryGetValue(key, out T value))
+            {
+                return value;
+            }
+
+            value = createValue();
+            dictionary.Add(key, value);
+            return value;
+        }
+    }
+}
