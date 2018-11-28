88783e1bb9291127f1ec054fab1034cde79360c2
Odonno <bottiau.david@laposte.net>
2018-11-28 11:28:01 +0100

    feat: create succinct library

---
+++ Converto.SuccincT/Main.cs
@@ -0,0 +1,23 @@
+﻿using SuccincT.Options;
+
+namespace Converto.SuccincT
+{
+    public static class Main
+    {
+        public static Option<T> TryCopy<T>(this T @object) where T : class
+        {
+            return @object.Copy().ToOption();
+        }
+
+        public static Option<T> TryWith<T, TProps>(this T itemToCopy, TProps propertiesToUpdate)
+            where T : class where TProps : class
+        {
+            return itemToCopy.With(propertiesToUpdate).ToOption();
+        }
+
+        public static Option<T> TryConvertTo<T>(this object @object) where T : class
+        {
+            return @object.ConvertTo<T>().ToOption();
+        }
+    }
+}
