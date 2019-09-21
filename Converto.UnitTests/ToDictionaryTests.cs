6312359b990a2fb631f44b25714a6ddc0074de24
Odonno <bottiau.david@laposte.net>
2019-09-21 22:48:45 +0200

    feat: add recursive mode to ToDictionary function

--- Converto.UnitTests/ToDictionaryTests.cs
+++ Converto.UnitTests/ToDictionaryTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using Xunit;
 
 namespace Converto.UnitTests
 {
@@ -8,6 +9,7 @@ namespace Converto.UnitTests
         {
             public string Name { get; set; }
             public string Color { get; set; }
+            public int? ProductionYear { get; set; }
             public Owner Owner { get; set; }
         }
 
@@ -38,10 +40,46 @@ namespace Converto.UnitTests
             var result = car.ToDictionary();
 
             // Assert
-            Assert.Equal(3, result.Count);
+            Assert.Equal(4, result.Count);
             Assert.Equal("Name of the car", result["Name"]);
             Assert.Equal("Red", result["Color"]);
+            Assert.Null(result["ProductionYear"]);
             Assert.Same(owner, result["Owner"]);
+        }
+
+        [Fact]
+        public void AnObject_Returns_ADictionaryInRecursiveMode()
+        {
+            // Arrange
+            var owner = new Owner
+            {
+                FirstName = "Freddy",
+                LastName = "Mercury"
+            };
+
+            var car = new Car
+            {
+                Name = "Name of the car",
+                Color = "Red",
+                ProductionYear = 1999,
+                Owner = owner
+            };
+
+            // Act
+            var result = car.ToDictionary(true);
+
+            // Assert
+            Assert.Equal(4, result.Count);
+            Assert.Equal("Name of the car", result["Name"]);
+            Assert.Equal("Red", result["Color"]);
+            Assert.Equal(1999, result["ProductionYear"]);
+
+            Assert.NotSame(owner, result["Owner"]);
+            Assert.IsType<Dictionary<string, object>>(result["Owner"]);
+
+            var ownerDictionary = result["Owner"] as Dictionary<string, object>;
+            Assert.Equal("Freddy", ownerDictionary["FirstName"]);
+            Assert.Equal("Mercury", ownerDictionary["LastName"]);
         }
     }
 }
--- Converto/Main.ToDictionary.cs
+++ Converto/Main.ToDictionary.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Converto
 {
@@ -11,21 +10,51 @@ namespace Converto
         /// </summary>
         /// <typeparam name="T">The type of the blueprint object.</typeparam>
         /// <param name="object">The object to use to create the dictionary.</param>
+        /// <param name="recursive">Indicates if we convert nested object in dictionary recursively.</param>
         /// <returns>Returns a dictionary with each object's property as a key/value pair.</returns>
-		public static Dictionary<string, object> ToDictionary<T>(this T @object) where T : class
+		public static Dictionary<string, object> ToDictionary<T>(this T @object, bool recursive = false) where T : class
+        {
+            return ToDictionary(@object, typeof(T), recursive);
+        }
+        private static Dictionary<string, object> ToDictionary(object @object, Type objectType, bool recursive = false)
         {
             if (@object == null)
                 return default;
 
-            var cachedTypeInfo = GetCachedTypeInfo(typeof(T));
+            var cachedTypeInfo = GetCachedTypeInfo(objectType);
             var result = new Dictionary<string, object>();
 
             foreach (var property in cachedTypeInfo.ReadableProperties)
             {
-                result.Add(
-                    property.Name,
-                    property.GetValue(@object)
-                );
+                if (recursive)
+                {
+                    bool hasNestedDictionary = 
+                        !property.PropertyType.IsPrimitive && 
+                        property.PropertyType != typeof(string) &&
+                        (!property.PropertyType.IsGenericType || property.PropertyType.GetGenericTypeDefinition() != typeof(Nullable<>));
+
+                    if (hasNestedDictionary)
+                    {
+                        result.Add(
+                           property.Name,
+                           ToDictionary(property.GetValue(@object), property.PropertyType, true)
+                        );
+                    }
+                    else
+                    {
+                        result.Add(
+                           property.Name,
+                           property.GetValue(@object)
+                        );
+                    }
+                }
+                else
+                {
+                    result.Add(
+                       property.Name,
+                       property.GetValue(@object)
+                    );
+                }
             }
 
             return result;
